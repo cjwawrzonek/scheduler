e58b012afc4bc39c6c23978de62f0dff459afa65
Christian Wawrzonek <cjwawrzonek@gmail.com>
2017-09-23 09:08:08 -0700

    initial commit

---
+++ launcher.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+###################################################
+# Script for launching the server.
+#
+# Launch from inside the appointment_scheduler dir
+###################################################
+
+# Launch the server in the background and disown it from this terminal
+# session.
+export FLASK_APP=itsjustflaskr.py
+flask run -h 0.0.0.0 -p 80 > server.log &
+disown
